0b9615fcfa29b928c0a5620ba508a8a001c87325
PetraVagi <vagi.p94@gmail.com>
2021-10-01 15:54:12 +0200

    Simple menu points initialized

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -4,9 +4,31 @@ import React from 'react';
 import Styled from 'styled-components';
 
 const MainContainer = Styled.div`
-    border: 1px white solid;
-    width: 100%;
-    height: 5%;
+    background-color: #534a4a;
+    display: flex;
+    justify-content: space-between;
+`
+
+const LogoContainer = Styled.div`
+    display: flex;
+    align-items: center;
+`
+
+const Menu = Styled.div`
+    display: flex;
+    flex:  1;
+    justify-content: flex-end;
+    padding: 0.25rem;
+`
+
+const MenuItem = Styled.div`
+    margin: 0;
+    padding: 0.5rem;
+`
+
+const UserAvatar = Styled.div`
+    display: flex;
+    align-items: center;
 `
 
 /* TODO: We need a media query here so that we can handle the lower resolutions for the perfect Menu Bar height */
@@ -14,7 +36,14 @@ const MainContainer = Styled.div`
 export default function MenuBar() {
     return (
         <MainContainer>
-            Whatever is this
+            <LogoContainer>
+                Pimp My Vocabulary
+            </LogoContainer>
+            <Menu>
+                <MenuItem>My words</MenuItem>
+                <MenuItem>Let's play</MenuItem>
+            </Menu>
+            <UserAvatar>avatar</UserAvatar>
         </MainContainer>
     )
 }
