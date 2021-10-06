5e856f9fdc0f2543801f762535092fa7e388c004
PetraVagi <vagi.p94@gmail.com>
2021-10-06 17:47:49 +0200

    Removed hardcoded data from PlayingCard: used state, interfaces and some functions instead

---
+++ src/dummyData/playingCard.ts
@@ -0,0 +1,41 @@
+export interface Owner {
+	name: string;
+	sex: "male" | "female";
+}
+
+export interface Word {
+	english: string;
+	hungarian: string[];
+	sentences: string[];
+	notes?: string;
+}
+
+interface Data {
+	owner: Owner;
+	word: Word;
+}
+
+export const data: Data = {
+	owner: {
+		name: "Petra",
+		sex: "female",
+	},
+	word: {
+		english: "ditch",
+		hungarian: ["árok", "csatorna", "körülárkol"],
+		sentences: ["Why do you have a ditch in front of your house?"],
+		notes: "I dont have notes here",
+	},
+};
+
+export const data_2: Data = {
+	owner: {
+		name: "Édi",
+		sex: "male",
+	},
+	word: {
+		english: "candid",
+		hungarian: ["őszinte", "nyílt", "pártatlan"],
+		sentences: ["His responses were remarkably candid.", "Candid is my new favourite word."],
+	},
+};
