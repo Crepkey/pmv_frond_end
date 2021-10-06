2371503f4a90cc816368ee4f46df40a73abc09d4
PetraVagi <vagi.p94@gmail.com>
2021-10-06 10:54:05 +0200

    Add new word: more html elements added

--- src/components/EditWord.tsx
+++ src/components/EditWord.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 /* Styles */
 import styled from 'styled-components';
-import { IoClose, IoAddCircleOutline } from 'react-icons/io5'
+import { IoClose } from 'react-icons/io5'
+import { MdOutlineAdd } from 'react-icons/md'
 
 const Card = styled.div`
-    background-color:   #aaa2a2;
+    border: 1px solid #b1bcc7;
     border-radius: 8px;
     margin: 16px;
     width: 30vw;
@@ -17,7 +18,8 @@ const Card = styled.div`
 `
 
 const CardHeader = styled.div`
-    background-color: #857f7f;
+    background-color: #ecebf0;
+    border-bottom: 1px solid #b1bcc7;
     padding: 0 16px;
     min-height: 3.5rem;
     display: flex;
@@ -29,7 +31,6 @@ const CardHeader = styled.div`
 const Icon = styled.div`
     display: flex;
     align-items: center;
-    color: #e4dcdc;
     padding-left: 16px;
 `
 
@@ -41,28 +42,131 @@ const CardBody = styled.div`
     flex: 1;
     min-width: 0;
     min-height: 0;
-    border: solid 1px black;
+`
+
+const Form = styled.form`
+    display: flex;
+    flex-direction: column;
 `
 
 const LighterCard = styled.div`
-    background-color:#c7bebe;
+    display: flex;
+    flex-direction: column;
+    background: #F7F6FC;
     border-radius: 8px;
-    padding: 8px;
+    padding: 20px;
     margin-bottom: 16px;
+    font-weight: 300;
 `
 
 const String = styled.input`
-    ::placeholder{
-        color: red;
-    }
+    display: flex;
+    flex: 1;
+    font-size: 1rem;
+    padding: 8px 12px;
     margin-bottom: 12px;
+    border: 1px #c7c7c7 solid;
+    border-radius: 20px;
+    color: grey;
+    ::placeholder{
+        color: #c7c7c7;
+    }
+    :focus{
+        outline: 1px #56abe3 solid;
+    }
 `
 
 const AddNewRow = styled.div`
     display: flex;
     justify-content: flex-start;
+    align-items: center;
+    font-size: 0.9rem;
 `
 
+const CircleButton = styled.button`
+	display:flex;
+    justify-content: center;
+    align-items: center;
+    height: 24px;
+    width: 24px;
+	border-radius:12px;
+    margin-right: 8px;
+	cursor:pointer;
+	color:#ffffff;
+	font-size:1rem;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #4751ae;
+    box-shadow:inset 0px 1px 0px 0px #bedffb;
+	background-color:#56abe3;
+	border: none;
+    :hover {
+	background:linear-gradient(to bottom, #527dcc 5%, #56abe3 100%);
+	background-color:#527dcc;
+    }
+    :active {
+        position:relative;
+        top:1px;
+    }
+`
+
+const Select = styled.select`
+    display: flex;
+    flex: 1;
+    font-size: 1rem;
+    padding: 8px 12px;
+    margin-bottom: 12px;
+    border: 1px #c7c7c7 solid;
+    border-radius: 20px;
+    color: grey;
+    ::placeholder{
+        color: #c7c7c7;
+    }
+    :focus{
+        outline: 1px #56abe3 solid;
+    }
+`
+
+const Textarea = styled.textarea`
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+    font-size: 1rem;
+    padding: 8px 12px;
+    margin-bottom: 12px;
+    border: 1px #c7c7c7 solid;
+    border-radius: 20px;
+    color: grey;
+    resize: none;
+    min-height: 5rem;
+    ::placeholder{
+        color: #c7c7c7;
+    }
+    :focus{
+        outline: 1px #56abe3 solid;
+    }
+`
+
+const Button = styled.div`
+    display: inline-block;
+    width: fit-content;
+    cursor: pointer;
+    color: #ffffff;
+    font-size: 1rem;
+    font-weight: bold;
+    padding: 6px 24px;
+    text-decoration: none;
+    border-radius: 15px;
+    border: none;
+    background-color: #51cb33;
+    :hover {
+        background: linear-gradient(to bottom, #a5cc52 5%, #b8e356 100%);
+        background-color:#a5cc52;
+    }
+    :active {
+        position:relative;
+        top:1px;
+    }
+`
 
 export default function EditWord() {
 
@@ -70,12 +174,35 @@ export default function EditWord() {
         <CardHeader>Add new word <Icon><IoClose /></Icon></CardHeader>
         <CardBody>
 
-            <String placeholder="English word or expression" />
+            <Form>
+                <String placeholder="English word or expression" />
 
-            <LighterCard>
-                <String placeholder="Hungarian meaning" />
-                <AddNewRow> <IoAddCircleOutline /> Add one more Hungarian meaning</AddNewRow>
-            </LighterCard>
+                <LighterCard>
+                    <String placeholder="Hungarian meaning" />
+                    <AddNewRow>
+                        <CircleButton><MdOutlineAdd size={12} /></CircleButton>
+                        Add one more Hungarian meaning
+                    </AddNewRow>
+                </LighterCard>
+
+                <LighterCard>
+                    <String placeholder="Example sentence" />
+                    <AddNewRow>
+                        <CircleButton><MdOutlineAdd size={12} /></CircleButton>
+                        Add one more example sentence
+                    </AddNewRow>
+                </LighterCard>
+
+                <Select placeholder="Type">
+                    <option>Word</option>
+                    <option>Expression</option>
+                </Select>
+
+                <Textarea placeholder="You can write your notes here"></Textarea>
+
+                <Button>Save</Button>
+            </Form>
+
         </CardBody>
     </Card>
 }
