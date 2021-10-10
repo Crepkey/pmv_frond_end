7ee380d96e21d3d0228555a7f1a388d482338f59
Crepkey <crepkey@gmail.com>
2021-10-10 13:10:26 +0200

    WordHandler works with deleted and active words

--- src/components/subComponents/myWords/wordHandler.tsx
+++ src/components/subComponents/myWords/wordHandler.tsx
@@ -1,14 +1,17 @@
+import React from "react";
+
 /* Interfaces */
 import { ExtendedWord } from "../../../utils/interfaces";
 
 /* Styles */
 import { BsSuitHeart, BsSuitHeartFill, BsPencil, BsTrash } from "react-icons/bs";
+import { IoArrowUndoOutline } from "react-icons/io5";
 import styled from "styled-components";
 
 const MainContainer = styled.div`
 	display: flex;
 	flex: 1;
-	justify-content: space-between;
+	justify-content: flex-end;
 	padding-right: 12px;
 `;
 
@@ -16,13 +19,30 @@ interface WordHandlerProps {
 	word: ExtendedWord;
 }
 export default function WordHandler({ word }: WordHandlerProps) {
-	return (
-		<MainContainer>
-			{word.favourite ? <BsSuitHeartFill size={25} /> : <BsSuitHeart size={25} />}
-			<BsPencil size={25} />
-			<BsTrash size={25} />
-		</MainContainer>
-	);
+	function calculateHandlerIcons() {
+		if (word.active === true) {
+			return (
+				<React.Fragment>
+					{word.favourite ? (
+						<BsSuitHeartFill size={25} style={{ marginRight: 12 }} />
+					) : (
+						<BsSuitHeart size={25} style={{ marginRight: 12 }} />
+					)}
+					<BsPencil size={25} style={{ marginRight: 12 }} />
+					<BsTrash size={25} />
+				</React.Fragment>
+			);
+		} else {
+			return (
+				<React.Fragment>
+					<IoArrowUndoOutline size={25} style={{ marginRight: 12 }} />
+					<BsTrash size={25} />
+				</React.Fragment>
+			);
+		}
+	}
+
+	return <MainContainer>{calculateHandlerIcons()}</MainContainer>;
 }
 
 /* TODO: Handler icons' hover FXs are missing */
