6d0633cedcaeef3a29ba81312d19f7cb1be244bb
Crepkey <crepkey@gmail.com>
2021-10-13 16:58:22 +0200

    Empty word object is in the utils file

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,24 @@
+import { WordWithScores } from "./interfaces";
+
 export function generateID() {
 	return Date.now();
 }
+
+export const emptyWord: WordWithScores = {
+	id: generateID(),
+	english: "",
+	hungarian: [""],
+	sentences: [""],
+	type: "word",
+	ownerId: 0,
+	memoryLevel: 0,
+	actualScore: 0,
+	scoreToAchieve: 0,
+	favourite: false,
+	notes: null,
+	deletionDate: null,
+	statistics: {
+		english: 0,
+		hungarian: [],
+	},
+};
