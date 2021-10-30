258948d3d16480f1f9f4eb627ba7a1a7beb58c46
Crepkey <crepkey@gmail.com>
2021-10-30 13:06:31 +0200

    Removing those interfaces which come from shared repo

--- src/components/subComponents/game/interfaces.ts
+++ src/components/subComponents/game/interfaces.ts
@@ -1,23 +1,6 @@
 import { WordWithScores } from "./../../../utils/interfaces";
 import { LanguageType } from "../../../utils/interfaces";
 
-export interface KnowledgeLevel {
-	language: LanguageType;
-	index?: number;
-	point: number;
-}
-
-export type ProgressColorType = "progressBlue" | "progressRed" | "progressGreen";
-
-export interface TagColor extends KnowledgeLevel {
-	color: ProgressColorType;
-}
-
-export interface GameStatistics {
-	english: boolean;
-	hungarian: boolean[];
-}
-
 export interface WordInGame extends WordWithScores {
 	wordToAsk: string;
 	wordToAnswer: string;
--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -4,31 +4,7 @@ export interface User {
 	id: number;
 }
 
-export type WordType = "word" | "expression";
-export type LanguageType = "english" | "hungarian";
 export type WordOperationType = "edit" | "delete" | "restore";
-
-export interface WordStatistics {
-	english: number;
-	hungarian: number[];
-}
-export interface Word {
-	id: number;
-	ownerId: number;
-	english: string;
-	hungarian: string[];
-	exampleSentences: string[];
-	notes: string | null;
-	type: WordType;
-	favourite: boolean;
-	deletionDate: Date | null;
-	memoryLevel: number;
-}
-export interface WordWithScores extends Word {
-	actualScore: number;
-	finalScore: number;
-	statistics: WordStatistics;
-}
 export interface Points {
 	[ownerId: number]: number;
 }
