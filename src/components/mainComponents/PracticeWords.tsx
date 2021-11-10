8fe8d7e85605bb875cd9ee24664288c7ade3cb78
Crepkey <crepkey@gmail.com>
2021-11-10 16:35:56 +0100

    Interfaces for dummy data are done

--- src/components/mainComponents/PracticeWords.tsx
+++ src/components/mainComponents/PracticeWords.tsx
@@ -1,3 +1,33 @@
+import { Word } from "sharedInterfaces";
+
+interface WordChoices {
+	correctAnswer: string;
+	wrongAnswer1: string;
+	wrongAnswer2: string;
+	wrongAnswer3: string;
+}
+interface WordRelation {
+	english: string;
+	hungarianMeanings: string[];
+}
+
+interface WordDefinition {
+	english: string;
+	definitions: string[];
+}
+
+interface gameData {
+	multipleChoiceGame: WordChoices[];
+	answerTypingGame: WordRelation[];
+	recognizeByTheRuleGame: WordDefinition[];
+}
+
+const dummyData = {
+	multipleChoiceGame: [{}],
+	answerTypingGame: [{}],
+	recognizeByTheRuleGame: [{}],
+};
+
 export default function PracticeWords() {
 	return <div>VALAMI</div>;
 }
