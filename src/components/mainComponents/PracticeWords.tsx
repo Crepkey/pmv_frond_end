76ef551fa019d07863bd97a1407f93a424ce0173
Crepkey <crepkey@gmail.com>
2021-11-10 16:53:27 +0100

    Initial dummy data is done

--- src/components/mainComponents/PracticeWords.tsx
+++ src/components/mainComponents/PracticeWords.tsx
@@ -1,6 +1,7 @@
 import { Word } from "sharedInterfaces";
 
 interface WordChoices {
+	question: string;
 	correctAnswer: string;
 	wrongAnswer1: string;
 	wrongAnswer2: string;
@@ -22,10 +23,47 @@ interface gameData {
 	recognizeByTheRuleGame: WordDefinition[];
 }
 
-const dummyData = {
-	multipleChoiceGame: [{}],
-	answerTypingGame: [{}],
-	recognizeByTheRuleGame: [{}],
+const dummyData: gameData = {
+	multipleChoiceGame: [
+		{
+			question: "What is the correct translation of the word: PINK?",
+			correctAnswer: "rózsaszín",
+			wrongAnswer1: "piros",
+			wrongAnswer2: "barna",
+			wrongAnswer3: "kék",
+		},
+		{
+			question: "What is the correct translation of the word: GRUELING?",
+			correctAnswer: "fárasztó",
+			wrongAnswer1: "álmos",
+			wrongAnswer2: "kabát",
+			wrongAnswer3: "macska",
+		},
+		{
+			question: "What is the correct translation of the word: PRAM?",
+			correctAnswer: "babakocsi",
+			wrongAnswer1: "patkány",
+			wrongAnswer2: "csicska",
+			wrongAnswer3: "mókua",
+		},
+	],
+	answerTypingGame: [
+		{ english: "willing", hungarianMeanings: ["hajlandó, készséges, önkéntes"] },
+		{ english: "assertion", hungarianMeanings: ["állítás", "kijelentés", "követelés"] },
+		{ english: "adamant", hungarianMeanings: ["hajthatatlan", "gyémántkeménységű anyag"] },
+	],
+	recognizeByTheRuleGame: [
+		{ english: "puppy", definitions: ["A young dog.", "A cheeky or arrogant boy or young man.", "A person or thing of a specified kind."] },
+		{
+			english: "cringing",
+			definitions: [
+				"Bent in fear or apprehension.",
+				"Obedient or attentive to an excessive or servile degree.",
+				"Experiencing embarrassment or disgust.",
+			],
+		},
+		{ english: "unwed", definitions: ["Not married"] },
+	],
 };
 
 export default function PracticeWords() {
