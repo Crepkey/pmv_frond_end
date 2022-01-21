bf5399ff8ae72676637a6d6355ee8d9bacdbdcdf
Crepkey <crepkey@gmail.com>
2022-01-21 16:45:10 +0100

    Restore homescreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 import { Route, Redirect, Switch } from "react-router-dom";
 
-// Components
+/* Components */
 import MenuBar from "./components/mainComponents/MenuBar";
 import MyWords from "./components/mainComponents/MyWords";
 import Home from "./components/mainComponents/Home";
 import Game from "./components/mainComponents/Game";
 import PracticeGrammar from "./components/mainComponents/PracticeGrammar";
+import PracticeWords from "./components/mainComponents/PracticeWords";
+
+/* Utils */
 import ToastHandler from "./components/generalComponents/toast/ToastHandler";
+
+/* Context */
 import { AppProvider } from "./AppContext";
 
-// Styles
+/* Styles */
 import styled from "styled-components";
-import PracticeWords from "./components/mainComponents/PracticeWords";
-import LoadingScreen from "./components/generalComponents/LoadingScreen";
 
 const MainContainer = styled.div`
 	display: flex;
@@ -32,7 +35,7 @@ function App() {
 					<Route path="/my-words" component={MyWords} />
 					<Route path="/practice/grammatical-structures" component={PracticeGrammar} />
 					<Route path="/lets-play" component={Game} />
-					<Route path="/" exact component={LoadingScreen} />
+					<Route path="/" exact component={Home} />
 					<Redirect to="/" />
 				</Switch>
 				<ToastHandler />
